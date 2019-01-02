f70f693f62a3e992fff2aaabffd00b1c4393e4cd
Ivan Mora <ivan@MacBook-Pro-de-Ivan-2.local>
2019-01-02 18:26:00 -0500

    Se agregan escenarios de respuestas con error para api_weather

--- features/support/api/BE.api.helper.rb
+++ features/support/api/BE.api.helper.rb
@@ -15,6 +15,18 @@ module Be_Api_Helper
     end
   end
 
+  def self.get_weather_info_key(city,key)
+    begin
+      api_url="api.openweathermap.org/data/2.5/weather?q="
+      service_url = api_url + city + "&appid=" +  key
+      get_info = {:url => service_url}
+      @current_response = API_Helper::send_get(get_info)
+      return @current_response
+    rescue => error
+      API_Helper::handle_error(__method__, service_url, error, "Error while trying to weather info")
+    end
+  end
+
 end
 
 World(Be_Api_Helper)
